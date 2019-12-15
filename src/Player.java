382811f02aa7f02e9d165c380722bea60749c2f3
Tumonbai <ztima90@gmail.com>
2019-12-15 22:35:04 +0600

    Initial commit

---
+++ src/Player.java
@@ -0,0 +1,9 @@
+public class Player {
+
+    private int bossHealth;
+    private int bossDamage;
+    private int[] heroesHealth;
+    private int[] heroesDamage;
+
+
+}
---
+++ src/Warrior.java
@@ -0,0 +1,22 @@
+public class Warrior extends Player{
+
+    private int defenceType = 60;
+    private String[] attackType = {"Physycal",
+            "Magical", "Kinetic", "Medical"};
+
+    public int getDefenceType() {
+        return defenceType;
+    }
+
+    public void setDefenceType(int defenceType) {
+        this.defenceType = defenceType;
+    }
+
+    public String[] getAttackType() {
+        return attackType;
+    }
+
+    public void setAttackType(String[] attackType) {
+        this.attackType = attackType;
+    }
+}
